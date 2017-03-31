1601f22107dad155f2b9f840436169fe82c5354d
jon-erikklint <jonerik.klint@gmail.com>
2017-03-31 13:31:40 +0300

    mockupdao

---
+++ BibTexKanta/src/main/java/database/MockupDAO.java
@@ -0,0 +1,26 @@
+package database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockupDAO<T> implements DAO<T>{
+
+    private List<T> ts;
+    
+    public MockupDAO(){
+        ts = new ArrayList<>();
+    }
+    
+    @Override
+    public List findAll() throws Exception {
+        return ts;
+    }
+
+    @Override
+    public boolean add(T t) throws Exception {
+        ts.add(t);
+        
+        return true;
+    }
+    
+}

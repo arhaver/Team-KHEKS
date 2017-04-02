b0118ec5200e1385a6d8aaaf7888efa84b64cfba
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-04-02 15:53:26 +0300

    Uusi main

--- BibTexKanta/src/main/java/Main.java
+++ BibTexKanta/src/main/java/Main.java
@@ -1,14 +1,34 @@
 
 import commandlineUI.BookAdder;
 import database.Database;
+import java.util.Scanner;
 
 
-public class Main {
-    
+public class Main {    
+      
     public static void main(String[] args){
-        Database db = new Database("SD");
         
+        Scanner reader = new Scanner (System.in);
+        Database db = new Database("SD");
         BookAdder ba = new BookAdder();
-        ba.addBookToDB(db);
+        
+        System.out.println("");
+        System.out.println("Lähdeviitteidenhallintaohjelma by Team-KHEKS.");
+        System.out.println("");
+        System.out.println("Tervetuloa!");
+        System.out.println("");
+        
+        while (true){
+            System.out.println("1 Lisää uusi kirja lähdeviitteisiin.");
+            System.out.println("Q Poistu ohjelmasta.");
+            System.out.println("Anna valinta: ");
+            String response = reader.nextLine();
+            
+            if (response.matches("1")){
+//                ba.addBookToDB(db);
+            } else if(response.matches("q") || response.matches("Q")){
+                break;                
+            }
+        }
     }
 }

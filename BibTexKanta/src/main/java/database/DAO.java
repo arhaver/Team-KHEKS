2d383a88d96d21599a47ac4fd21f96a01a35fad0
jon-erikklint <jonerik.klint@gmail.com>
2017-04-03 13:21:18 +0300

    testejä ja inmemory

--- BibTexKanta/src/main/java/database/DAO.java
+++ BibTexKanta/src/main/java/database/DAO.java
@@ -5,8 +5,8 @@ import java.util.List;
 public interface DAO<T> {
     
     //Hakee tietokannasta kaikki tyyppiä T olevat otukset
-    public List<T> findAll() throws Exception;
+    public List<T> findAll();
     
     //Lisää tietokantaan annetun T:n
-    public boolean add(T t) throws Exception; 
+    public boolean add(T t); 
 }

5320a0bd7cf6ce7bcd869fed960577d3a1cef86b
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-04-24 22:55:59 +0300

    InproceedinsRef-tehty, testit kesken

---
+++ BibTexKanta/src/main/java/reference/InproceedingsRef.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reference;
+
+/**
+ *
+ * @author Arto
+ */
+public class InproceedingsRef extends AbstractReference implements Reference {
+
+    public InproceedingsRef() {
+        fields.put("booktitle", null);
+        fields.put("pages", null);
+        fields.put("publisher", null);
+        fields.put("address", null);
+    }
+    
+    public InproceedingsRef(String authors, String title, String booktitle, String year, String publisher, String address, String pages) {
+        fields.put("authors", authors);
+        fields.put("title", title);
+        fields.put("booktitle", booktitle);
+        fields.put("publisher", publisher);  
+        fields.put("address", address);
+        fields.put("pages", pages);
+        
+        try {
+            this.year = Integer.getInteger(year);
+        } catch (Error e) {
+
+        }
+    }
+    
+
+    @Override
+    public boolean readyForDb() {
+        return (isValidString("title", fields.get("title")) && isValidString("authors", fields.get("authors")) && isValidYear(year) && isValidString("booktitle", fields.get("booktitle")));
+    }
+    
+}

69db7d8dce32afb108f7e4ad029319bcb424316f
Arto Haverinen <arto.haverinen@helsinki.fi>
2017-04-25 11:35:40 +0300

    InproceedingsRef bibtexid

--- BibTexKanta/src/main/java/reference/InproceedingsRef.java
+++ BibTexKanta/src/main/java/reference/InproceedingsRef.java
@@ -18,13 +18,14 @@ public class InproceedingsRef extends AbstractReference implements Reference {
         fields.put("address", null);
     }
     
-    public InproceedingsRef(String authors, String title, String booktitle, String year, String publisher, String address, String pages) {
+    public InproceedingsRef(String authors, String title, String booktitle, String year, String publisher, String address, String pages, String bibtexid) {
         fields.put("authors", authors);
         fields.put("title", title);
         fields.put("booktitle", booktitle);
         fields.put("publisher", publisher);  
         fields.put("address", address);
         fields.put("pages", pages);
+        fields.put("bibTexId", bibtexid);
         
         try {
             this.year = Integer.getInteger(year);
